d669eb79d74f518389608f0d74e0084ee3112a1b
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2016-05-01 06:18:19 +0200

    SidebarJS

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,4 +1,4 @@
-/* jshint -W030  */ /* jshint -W040 */
+/* jshint -W003  *//* jshint -W030  *//* jshint -W040 */
 
 window.Sidebar = (function(window, document) {
   'use strict';
@@ -6,6 +6,33 @@ window.Sidebar = (function(window, document) {
   const IS_VISIBLE = 'is-visible';
   const TRANSITION = 'transition: all';
   const TRANSLATE = 'transform: translate';
+
+  class Sidebar {
+    constructor(config) {
+      this.$component = document.querySelector(config ? config.component : '.js-sidebar');
+      this.$container = document.querySelector(config ? config.container : '.js-sidebar--container');
+      this.$background = document.querySelector(config ? config.background : '.js-sidebar--background');
+      this.$open = document.querySelector(config ? config.showElement : '.js-sidebar--open');
+      
+      this.$container.addEventListener('touchstart', _onTouchStart.bind(this));
+      this.$container.addEventListener('touchmove', _onTouchMove.bind(this));
+      this.$container.addEventListener('touchend', _onTouchEnd.bind(this));
+      this.$background.addEventListener('click', this.close.bind(this));
+      this.$open.addEventListener('click', this.open.bind(this));
+    }
+
+    open() {
+      _element(this.$container, [TRANSITION + '.3s ease', TRANSLATE + '(0, 0)']);
+      if(!this.$component.classList.contains(IS_VISIBLE)) {
+        this.$component.classList.add(IS_VISIBLE);
+      }
+    }
+
+    close() {
+      _element(this.$container, [TRANSITION + '.3s ease', TRANSLATE + '(-100%, 0)']);
+      this.$component.classList.remove(IS_VISIBLE);
+    }
+  }
 
   function _vendorify(el, prop, val) {
     const Prop = prop.charAt(0).toUpperCase() + prop.slice(1);
@@ -39,32 +66,6 @@ window.Sidebar = (function(window, document) {
 
   function _onTouchEnd() {
     this.$container.touchMove > (this.$container.clientWidth/4) ? this.close() : this.open();
-  }
-
-  class Sidebar {
-    constructor(config) {
-      this.$component = document.querySelector(config ? config.component : '.js-sidebar');
-      this.$container = document.querySelector(config ? config.container : '.js-sidebar--container');
-      this.$background = document.querySelector(config ? config.background : '.js-sidebar--background');
-      this.$open = document.querySelector(config ? config.showElement : '.js-sidebar--open');
-      this.$container.addEventListener('touchstart', _onTouchStart.bind(this));
-      this.$container.addEventListener('touchmove', _onTouchMove.bind(this));
-      this.$container.addEventListener('touchend', _onTouchEnd.bind(this));
-      this.$background.addEventListener('click', this.close.bind(this));
-      this.$open.addEventListener('click', this.open.bind(this));
-    }
-
-    open() {
-      _element(this.$container, [TRANSITION + '.3s ease', TRANSLATE + '(0, 0)']);
-      if(!this.$component.classList.contains(IS_VISIBLE)) {
-        this.$component.classList.add(IS_VISIBLE);
-      }
-    }
-
-    close() {
-      _element(this.$container, [TRANSITION + '.3s ease', TRANSLATE + '(-100%, 0)']);
-      this.$component.classList.remove(IS_VISIBLE);
-    }
   }
 
   return Sidebar;
