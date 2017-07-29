8c278a4a4289dbb8c613db4a73973979ec0bc209
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2017-07-29 02:25:44 +0200

    build(rollup): defined exports type

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import npm from 'rollup-plugin-node-resolve';
 export default {
   entry: 'src/index.js',
   moduleName: 'SidebarJS',
+  exports: 'named',
   targets: [
     {
       dest: 'dist/sidebarjs.js',
