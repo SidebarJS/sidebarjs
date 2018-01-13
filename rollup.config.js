412c26bf8bfb40cad711774580c89b5d303f4edd
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-01-13 01:44:11 +0100

    chore(npm): update devDep

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
 import commonjs from 'rollup-plugin-commonjs';
 import npm from 'rollup-plugin-node-resolve';
+import * as packageJson from './package.json';
+
+const info = `/*
+ * ${packageJson.library}
+ * Version ${packageJson.version}
+ * ${packageJson.homepage}
+ */
+`;
 
 export default {
   input: 'src/index.js',
-  name: 'SidebarJS',
-  output: [{
+  output: {
+    name: 'SidebarJS',
     file: 'dist/sidebarjs.js',
     format: 'umd',
     exports: 'named',
-  }],
+    banner: info,
+  },
   plugins: [
     npm({
       main: true,
