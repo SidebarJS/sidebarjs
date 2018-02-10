ceae89f494ca8f415fa958013879b61bcf29e5b4
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-02-10 20:02:13 +0100

    style(test): linter

--- test/static.spec.ts
+++ test/static.spec.ts
@@ -1,5 +1,5 @@
-import { HTMLSidebarElement } from './../index';
-import { SidebarElement } from './../src/sidebarElement';
+import {HTMLSidebarElement} from '../index';
+import {SidebarElement} from '../src';
 
 describe('Static', () => {
   beforeEach(() => {
@@ -7,7 +7,7 @@ describe('Static', () => {
   });
 
   describe('SidebarElement.create', () => {
-    it('Should create a div element with specific attribute', () => {
+    test('Should create a div element with specific attribute', () => {
       const elem = SidebarElement.create('hello');
       expect(elem).toBeInstanceOf(HTMLDivElement);
       expect(elem.attributes['hello']).toBeDefined();
@@ -16,7 +16,7 @@ describe('Static', () => {
   });
 
   describe('SidebarElement.vendorify', () => {
-    it('Should add css vendor prefixes', () => {
+    test('Should add css vendor prefixes', () => {
       const elem = document.createElement('div');
       SidebarElement.vendorify(elem, 'transform', `translate(0, 0)`);
       expect(elem.style['WebkitTransform']).toBeDefined();
@@ -27,31 +27,31 @@ describe('Static', () => {
   });
 
   describe('SidebarElement.elemHasListener', () => {
-    it('Should return boolean value that notify if element has SidebarElement listeners', () => {
+    test('Should return boolean value that notify if element has SidebarElement listeners', () => {
       document.body.innerHTML = `
         <button sidebarjs-toggle></button>
         <div sidebarjs></div>
       `;
-      const elem = <HTMLSidebarElement>document.querySelector('button[sidebarjs-toggle]');
+      const elem = <HTMLSidebarElement> document.querySelector('button[sidebarjs-toggle]');
       const hasFalseListener = SidebarElement.elemHasListener(elem);
       expect(hasFalseListener).toBe(false);
-      new SidebarElement();
+      const sidebarjs = new SidebarElement();
       const hasTrueListener = SidebarElement.elemHasListener(elem);
       expect(hasTrueListener).toBe(true);
     });
 
-    it('Should set value to HTMLSidebarElement and return it', () => {
+    test('Should set value to HTMLSidebarElement and return it', () => {
       document.body.innerHTML = `
         <button sidebarjs-toggle></button>
         <div sidebarjs></div>
       `;
-      const elem = <HTMLSidebarElement>document.querySelector('button[sidebarjs-toggle]');
+      const elem = <HTMLSidebarElement> document.querySelector('button[sidebarjs-toggle]');
       const hasFalseListener = SidebarElement.elemHasListener(elem);
       expect(hasFalseListener).toBe(false);
       const isSetToTrueListener = SidebarElement.elemHasListener(elem, true);
       expect(isSetToTrueListener).toBe(true);
       const isSetToFalseListener = SidebarElement.elemHasListener(elem, false);
       expect(isSetToFalseListener).toBe(false);
-    })
+    });
   });
 });
